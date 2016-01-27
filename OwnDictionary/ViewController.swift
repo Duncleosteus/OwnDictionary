d72402b7c3b3818ef5b3d21186c94e7d98e55283
Duncleosteus <rodin@rodins-macbook-pro.local>
2016-01-27 11:02:27 +0200

    Initial Commit

---
+++ OwnDictionary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OwnDictionary
+//
+//  Created by Rodin Artem on 27.01.16.
+//  Copyright © 2016 Rodin Artem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
